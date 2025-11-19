d46eb7ad038ff07504ab2b6dba6c22989c13c450
lcarlo23 <160224871+lcarlo23@users.noreply.github.com>
2025-11-19 11:44:16 +0100

    Fixed timestamp not showing updated time

--- chamber/scripts/form.mjs
+++ chamber/scripts/form.mjs
@@ -3,6 +3,6 @@ const timestamp = document.getElementById('timestamp');
 
 export default function sendTime() {
     form.addEventListener('submit', () => {
-        timestamp.value = document.lastModified;
+        timestamp.value = new Date().toLocaleString();
     })
 }
